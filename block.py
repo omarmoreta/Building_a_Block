401a0425abd9423263c167679aa8c21d287ad4d0
Omar <o.moreta@gmail.com>
2023-04-25 20:53:12 -0400

    add a hash block method to block class

--- block.py
+++ block.py
@@ -15,29 +15,27 @@ class Block:
     hash: Any
     timestamp: str = datetime.utcnow().strftime("%H:%M:%S")
 
+    # Create a function to hash the block data
+    def hash_block(self):
+        sha = hashlib.sha256()
+        data = str(self.data).encode()
+        sha.update(data)
+        creator_id = str(self.creator_id).encode()
+        sha.update(creator_id)
+        timestamp = str(self.timestamp).encode()
+        sha.update(timestamp)
+        return sha.hexdigest()
+
 
 # Write some markdown titles with streamlit on the app
 st.markdown("# PyBlock")
-st.markdown("## Store Data in a Block and hash data")
+st.markdown("## Store and hash data in a block")
 
 # Receive input data from the user to put into the blockchain
 input_data = st.text_area("Block Data")
 
-# Create a function to hash the input data
-
-
-def hash_data(data):
-    sha = hashlib.sha256()
-    encoded_data = str(data).encode()
-    sha.update(encoded_data)
-    return sha.hexdigest()
-
-
 # Create a button and add the a newly created Block object with all the date inside
-if st.button("Add Block"):
-    new_block = Block(
-        data=input_data,
-        creator_id=42,
-        hash=hash_data(input_data)
-    )
-    st.write(new_block)
+if st.button("Add and Hash Block"):
+    new_block = Block(data=input_data, creator_id=42, hash='')
+    hashed_block = new_block.hash_block()
+    st.write("New Block Fingerprint", hashed_block)
