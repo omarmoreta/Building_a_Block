ba90fc66ed0d2eac05295b4a07fa493eb73e50cd
Omar <o.moreta@gmail.com>
2023-04-25 20:09:20 -0400

    added the hashing function for the block

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 In this activity, you’ll build a Streamlit application that accepts user input and then stores that input in a `Block` data class.
 The instructions for this activity are divided into the following high-level steps:
+
 1. Create a data class for storing data from a user.
 2. Create a Streamlit component to accept user input.
 3. Create a button for storing and displaying the user input.
--- block.py
+++ block.py
@@ -1,97 +1,43 @@
-################################################################################
 # Imports
 import streamlit as st
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Any
+import hashlib
 
-################################################################################
-# Step 1:
-# Create a Data Class for Storing Data from a User
-
-# In this section, you’ll create a data class named `Block` for storing data
-# from a user. To do so, complete the following steps:
-# 1. Define a class named `Block` and add the `@dataclass` decorator.
-# 2. Inside the data class, define an attribute named `data` with a type of `Any`.
-# 3. Inside the data class, define an attribute named `creator_id` with a type
-# of `int`.
-# 4. Inside the data class, define an attribute name `timestamp` with a type of
-# `str`.
-# 5. Assign a default value to the `timestamp` attribute by using the following
-# code: `datetime.utcnow().strftime("%H:%M:%S")`
+# Create a Block class to store the date, creator_id, hash, and timestamp
 
 
-# @TODO
-# Define a class `Block` and add the `@dataclass` decorator.
 @dataclass
 class Block:
-
-    # @TODO:
-    # Define an attribute named `data` with a type of `Any`.
     data: Any
-
-    # @TODO:
-    # Define an attribute named `creator_id` with a type of `int`.
     creator_id: int
-
-    # @TODO:
-    # Define an attribute name `timestamp` with a type of `str`.
-    # Use the following code to set the value:
-    # `datetime.utcnow().strftime("%H:%M:%S")`
+    hash: Any
     timestamp: str = datetime.utcnow().strftime("%H:%M:%S")
 
 
-# Create the application headers using markdown strings.
+# Write some markdown titles with streamlit on the app
 st.markdown("# PyBlock")
-st.markdown("## Store Data in a Block")
+st.markdown("## Store Data in a Block and hash data")
 
-################################################################################
-# Step 2:
-# Create a Streamlit Component to Accept User Input
+# Receive input data from the user to put into the blockchain
+input_data = st.text_area("Block Data")
 
-# @TODO:
-# Referencing the Streamlit library, use the `text_input` function and pass the
-# parameter "Block Data".
-input_data = st.text_input("Block Data")
+# Create a function to hash the input data
 
-################################################################################
-# Step 3:
-# Create a Button for Storing and Displaying the User Input
 
-# In this section, you’ll create a button that will store the user input in an
-# instance of the `Block` data class and then display it to the user. To do so,
-# complete the following steps:
-# 1. Create a Streamlit `button`, and pass the “Add Block” parameter to it.
-# 2. In the button statement, create an instance of the `Block` data class. Use
-# the user input from the preceding “Step 2” section for the data attribute,
-# and use the integer 42 for the creator ID.
-# Hint: Create an instance of the Block dataclass using the following code:
-# `new_block = Block(data=input_data, creator_id=42)`
-# 3. Use the `st.write` function to display the new block.
+def hash_data(data):
+    sha = hashlib.sha256()
+    encoded_data = str(data).encode()
+    sha.update(encoded_data)
+    return sha.hexdigest()
 
-# @TODO:
-# Create a Streamlit `button`, and pass the “Add Block” parameter to it.
+
+# Create a button and add the a newly created Block object with all the date inside
 if st.button("Add Block"):
-
-    # @TODO:
-    # Create an instance of the `Block` data class called `new_block`
-    # Use the user input from Step 2 for the `data` attribute
-    # Use the integer 42 for the `creator_id`
-    new_block = Block(data=input_data, creator_id=42)
-
-    # @TODO:
-    # Use the `st.write` function to display the new block.
+    new_block = Block(
+        data=input_data,
+        creator_id=42,
+        hash=hash_data(input_data)
+    )
     st.write(new_block)
-
-################################################################################
-# Step 4:
-# Test the application.
-
-# Complete the following steps:
-# 1. In the terminal, navigate to the `Unsolved` folder for this activity.
-# 2. Be sure that your Conda development environment is active.
-# 3. Run the Streamlit app in the terminal by using `streamlit run app.py`.
-# 4. Type some input text in the text box, and then click the Add Block button.
-# 5. Confirm that an instance of the `Block` class that appears on the page.
-
-################################################################################
